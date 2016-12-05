6d8df4acfd4e150daa8dbe6b1977c83ca7b2b610
Vincent Lemeunier <vincent.lemeunier+git@gmail.com>
2016-12-05 14:44:22 +0100

    Wip

--- quote.js
+++ quote.js
@@ -1,9 +1,47 @@
+const moment = require('moment');
+// {
+// "Country": "PL",
+// "departureDate" : "2016-11-15",
+// "returnDate" : 2016-12-09,
+// "travellerAges" : [32,39]
+// "options" : ["MedicalConditions"],
+// "Cover" :"Basic"
+// }
+
+function validateCountry(country) {
+  return true;
+}
+
+function validateOptions(options) {
+  return true;
+}
+
+function validateCover(cover) {
+  return cover === 'Basic';
+}
+
+function validateDates(departureDateStr, returnDateStr) {
+  // TODO
+  return true;
+}
+
+function validateQuote(quote) {
+  return validateCountry(quote.country)
+    && validateOptions(quote.options)
+    && validateCover(quote.cover)
+    && validateDates(quote.departureDate, quote.returnDate);
+}
+
 function quoteHandler(req, res) {
-  console.log('Quote endpoint was called !', req.body);
-  // res.send({
-  //   quote: 0
-  // });
-  res.sendStatus(400);
+  const quote = req.body || {};
+
+  console.log('Quote endpoint was called !', quote);
+
+  if (!validateQuote(quote)) {
+    return res.sendStatus(400);
+  }
+
+  res.sendStatus(200);
 }
 
 module.exports = quoteHandler;
