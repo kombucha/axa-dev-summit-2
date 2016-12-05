12ce3f448685ebcc8e8de7fde510602e9d77d94e
Vincent Lemeunier <vincent.lemeunier+git@gmail.com>
2016-12-05 14:02:02 +0100

    Permanent 400

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.sendStatus(400));
 
 app.listen(8080);
