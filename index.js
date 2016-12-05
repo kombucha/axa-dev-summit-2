6780490b743e82de19a90301c400952c1865ebda
Vincent Lemeunier <vincent.lemeunier+git@gmail.com>
2016-12-05 15:37:55 +0100

    Added debug middleware

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const feedbackHandler = require('./feedback');
 const quoteHandler = require('./quote');
 const logHandler = require('./log');
 
+const debugMiddleware = require('./debugMiddleware');
+
 const app = express();
 
+app.use(debugMiddleware);
 app.use(bodyParser.json());
 
 app.post('/quote', quoteHandler);
