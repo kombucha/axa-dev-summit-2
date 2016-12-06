114cbd697c684ea044d575a4a2a966d0a14493ec
Andreas Schramm <Andreas.jabs@gmail.com>
2016-12-06 15:22:33 +0100

    end to end tests

--- services/romanService_as.test.js
+++ services/romanService_as.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const romanService = require('./romanService');
+const romanService = require('./roman');
 
 describe('#romanPrice()', function() {
 
@@ -13,20 +13,20 @@ describe('#romanPrice()', function() {
       assert.equal(3*1, romanService.toRomanPrice(romanService.toRomanNumerals(3)));
     });
 
-    // 4  is IV is 4.4-1
-    it('should return 3.4 if 4', function() {
-      assert.equal(4.4-1, romanService.toRomanPrice(romanService.toRomanNumerals(4)));
+    // 4  is IV is 4.2-1
+    it('should return 3.2 if 4', function() {
+      assert.equal(4.2-1, romanService.toRomanPrice(romanService.toRomanNumerals(4)));
     });
 
-    // 5  is V is 4.4
-    it('should return 4.4 if 5', function() {
-      assert.equal(4.4, romanService.toRomanPrice(romanService.toRomanNumerals(5)));
+    // 5  is V is 4.2
+    it('should return 4.2 if 5', function() {
+      assert.equal(4.2, romanService.toRomanPrice(romanService.toRomanNumerals(5)));
     });
 
 
-    // 7  is VII is 4.4 + 2*1
+    // 7  is VII is 4.2 + 2*1
     it('should return 6.4 if 7', function() {
-      assert.equal(4.4 + 2*1, romanService.toRomanPrice(romanService.toRomanNumerals(7)));
+      assert.equal(4.2 + 2*1, romanService.toRomanPrice(romanService.toRomanNumerals(7)));
     });
 
 
@@ -35,16 +35,23 @@ describe('#romanPrice()', function() {
       assert.equal(8.4 -1, romanService.toRomanPrice(romanService.toRomanNumerals(9)));
     });
 
-    // 14  is XIV is 8.4 -1 + 4.4
-    it('should return 7.4 if 9', function() {
-      assert.equal(8.4 -1 + 4.4, romanService.toRomanPrice(romanService.toRomanNumerals(14)));
+    // 14  is XIV is 8.4 -1 + 4.2
+    it('should return 11.8 if 14', function() {
+      assert.equal(8.4 -1 + 4.2, romanService.toRomanPrice(romanService.toRomanNumerals(14)));
+    });
+
+    // 39  is XXXIX is 4*8.4 -1
+    it('should return '+ (4*8.4 -1+8.4) +' if 39', function() {
+      assert.equal(3*8.4 -1 + 8.4, romanService.toRomanPrice(romanService.toRomanNumerals(39)));
     });
 
 
-    // 14  is XIV is 8.4 -1 + 4.4
-    it('should return 7.4 if 9', function() {
-      assert.equal(8.4 -1 + 4.4, romanService.toRomanPrice(romanService.toRomanNumerals(14)));
+    // 39  is XXXIX is 4*8.4 -1
+    it('should return error if -1', function() {
+      assert.throws( romanService.toRomanPrice(romanService.toRomanNumerals(-1)));
     });
+
+
 
 
 });
